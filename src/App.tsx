37f7a871b4f992286efb942fb73f9bf31ebda4d6
Jorge Lazaro <skaterhater.16772@gmail.com>
2026-03-02 14:20:49 -0600

    add github pages deployment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-4xl font-bold">tailwind works</p>
+    </div>
+  );
+}
+
+export default App;
